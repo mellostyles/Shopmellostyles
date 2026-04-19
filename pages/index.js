6b6c4bf77d2cf049a493e2a4513f1abd1275c49f
mellostyles <shopmellostyles@outlook.com>
2026-04-19 11:13:55 -0400

    Create ShopMelloStyles store component in pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ShopMelloStyles = () => {
+  return (
+    <div>
+      {/* Stripe Integration */}
+      {/* Product Catalog */}
+      {/* Cart */}
+      {/* One-Click Upsell Modal */}
+    </div>
+  );
+};
+
+export default ShopMelloStyles;
+
+// Stripe public key
+const stripePublicKey = 'pk_test_51TNwZULQ4IJ5ksP84uMHB6NpNYGKlyyJnNrPqv0A7DpAMic7bkMaj0w88qKkcwUskon0zzbbmRSlAezXAymY2U8qRRtLzV2';
